a224dd9eb4411ed40bd487e55f4dc0fe29e00f47
Alex <mr.alex.grand@mail.ru>
2021-12-18 00:07:45 +0300

    add app to repo

---
+++ XplatformProject/XplatformProject/XplatformProject/HomeWorkPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XplatformProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeWorkPage : ContentPage
+    {
+        public HomeWorkPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
